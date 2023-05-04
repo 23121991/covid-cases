e1b0e7403e7321c73e1b9b7f059a2551c8c5bf54
SARAVANAN R <108886356+23121991@users.noreply.github.com>
2023-05-04 12:43:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-var div = document.createElement("div") div.style.textAlign = "center";
+var div=document.createElement("div");
+div.style.textAlign='center';
 
-var input = document.createElement("input"); input.setAttribute("type", "text"); input.setAttribute("id", "country");
+var input=document.createElement("input");
+input.setAttribute("type","text");
+input.setAttribute("id","country");
 
-var button = document.createElement("button"); button.setAttribute("type", "button"); button.innerHTML = "Search"; button.addEventListener("click",foo);
+var button=document.createElement("button");
+button.setAttribute("type","button");
+button.setAttribute("class","btn btn-primary");
+button.innerHTML="Search";
+button.addEventListener("click",foo);
 
-let active = document.createElement("div"); active.setAttribute("Id", "Active"); let Recovered = document.createElement("div"); Recovered.setAttribute("Id", "Recover"); let Deaths = document.createElement("div"); Deaths.setAttribute("Id", "Deaths");
+let active=document.createElement("div");
+active.setAttribute("id","active");
 
-div.append(input,button,active,Recovered,Deaths); document.body.append(div);
+let recovered=document.createElement("div");
+recovered.setAttribute("id","recovered");
 
-async function foo() {
+let deaths=document.createElement("div");
+deaths.setAttribute("id","deaths");
 
-let res = document.getElementById("country").value;
-var url = `https://api.covid19api.com/dayone/country/${res}`;
+div.append(input,button,active,recovered,deaths);
+document.body.append(div);
 
-let result = await fetch(url);
-let result1 = await result.json();
-let index = result1.length-1;
-console.log(result1[index].Active);
-active.innerHTML =`total Active Cases:${result1[index].Active}`;
-
-console.log(result1[index].Recovered);
-Recovered.innerHTML =`total Recovered Cases:${result1[index].Recovered}`;
-
-console.log(result1[index].Deaths);
-Deaths.innerHTML =`total Deaths Cases:${result1[index].Deaths}`;
-
+async function foo(){
+    let res=document.getElementById("country").value;
+    var url=`https://api.covid19api.com/dayone/country/${res}`;
+    
+    let result=await fetch(url);
+    let result1=await result.json();
+    var index=result1.length-1;
+    console.log(url);
+    active.innerHTML=`Total Active Cases: ${result1[index].Active}`;
+    recovered.innerHTML=`Total Recovered Cases: ${result1[index].Recovered}`;
+    deaths.innerHTML=`Total Deaths Cases: ${result1[index].Deaths}`;
 }
